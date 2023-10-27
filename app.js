43805e6dd361526ea96cc0ec8b0208a61ecc24c7
Jack L <jackktlam@gmail.com>
2023-10-27 17:25:46 +0100

    initial changes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
-})
+  const holes = document.querySelectorAll(".hole");
+  // will make an arraylike nodelist of the holes
+  // will the const make it not update when i add more?
+
+  const scoreDisplay = document.getElementById("score-value");
+  const timerDisplay = document.getElementById("timer-value");
+});
