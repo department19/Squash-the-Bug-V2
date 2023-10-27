3f086e2cd33e86b1152f3663d5dded1d5308674f
Jack Lam <jackktlam@gmail.com>
2023-10-27 20:37:10 +0100

    bound all trackable elements to JS

--- app.js
+++ app.js
@@ -5,4 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const scoreDisplay = document.getElementById("score-value");
   const timerDisplay = document.getElementById("timer-value");
+  const bug = document.createElement("img");
+  bug.setAttribute("src", "");
+  bug.id = "bug";
+
+  let gameTimeout = 0;
+  let score;
+  let time;
+  let isGameRunning = false;
+
+  function spawnBug() {
+    
+  }
+
+  function resetGame() {
+    score = 0;
+    time = 30;
+    scoreDisplay.textContent = score;
+    timerDisplay.textContent = time;
+  }
 });
